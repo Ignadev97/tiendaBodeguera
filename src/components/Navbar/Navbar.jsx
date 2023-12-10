cb9399fb44955f1682a459993127bb4572e851b9
Ignacio Tello <ignacio.tello.dev@gmail.com>
2023-12-10 13:34:57 -0300

    primer commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <ul className='flex bg-slate-500 h-12 justify-evenly items-center space-x-11'>
+          <li className='text-extralight'>Inicio</li>
+
+          <li>Contacto</li>
+
+          <li>Ayuda</li>
+        </ul>
+    </>
+  )
+}
+
+export default Navbar
